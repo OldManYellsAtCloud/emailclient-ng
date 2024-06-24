1d2978c4355f3b4dbf5a1728e2a61ddb4e3a3b2b
Gyorgy Sarvari <skandigraun@gmail.com>
2024-06-24 17:24:49 +0200

    switch to decicated user account if it was started with incorrect one

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,50 @@
 #include <QQmlApplicationEngine>
 #include <QtWebEngineQuick/QtWebEngineQuick>
 
+#include <pwd.h>
 
 #include "qml_models/modelfactory.h"
 #include "periodicdatafetcher.h"
+#include <loglibrary.h>
+
+
+
+void checkProcessOwner(int argc, char *argv[]){
+    MailSettings settings{};
+    std::string applicationUser = settings.getApplicationUser();
+    auto currentUid = getuid();
+    auto currentUser = getpwuid(currentUid);
+
+    std::string currentUserName {currentUser->pw_name};
+
+    if (currentUserName.compare(applicationUser) != 0){
+        LOG("Running with incorrect user: {} Attempting to switch to application user.", currentUserName);
+        auto appUserUid = getpwnam(applicationUser.c_str());
+
+        if (!appUserUid){
+            ERROR("Could not find application user! Error: {}", strerror(errno));
+            exit(1);
+        }
+
+        if (setgid(appUserUid->pw_gid) < 0){
+            ERROR("Could not set GID to application user's group! Error: {}", strerror(errno));
+            exit(1);
+        }
+
+        if(setuid(appUserUid->pw_uid) < 0){
+            ERROR("Could not set UID to application user! Error: {}", strerror(errno));
+            exit(1);
+        }
+
+        execv(argv[0], argv);
+    } else {
+        LOG("Already running as application user.");
+    }
+}
 
 int main(int argc, char *argv[])
 {
+    checkProcessOwner(argc, argv);
     qmlRegisterType<ModelFactory>("sgy.gspine.mail", 1, 0, "ModelFactory");
     qmlRegisterType<PeriodicDataFetcher>("sgy.gspine.mail", 1, 0, "PeriodicDataFetcher");
     QtWebEngineQuick::initialize();
